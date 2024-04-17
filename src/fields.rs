505d63da49a3586e095c58e634c3ade37f3dda09
Richard Frankel <rfrankel@roblox.com>
2024-04-17 18:14:04 -0500

    Fix binary serialization bugs.
    Summary: Added missing nil checks for a few field types during binary serialization.
    
    Test Plan: Added unit test coverage.

--- src/fields.rs
+++ src/fields.rs
@@ -183,11 +183,11 @@ impl FieldGenerator<'_> {
 
             FieldKind::Single(field) => {
                 if self.map_type().is_some() {
-                    return format!("next({this}) ~= nil");
+                    return format!("{this} and next({this}) ~= nil");
                 }
 
                 if field.label.is_some() && field.label() == Label::Repeated {
-                    return format!("#{this} > 0");
+                    return format!("{this} and #{this} > 0");
                 }
 
                 // TODO: Remove default branch and explicitly type everything out
@@ -204,13 +204,13 @@ impl FieldGenerator<'_> {
                     | Type::Fixed64
                     | Type::Float
                     | Type::Double => {
-                        format!("{this} ~= 0")
+                        format!("{this} ~= nil and {this} ~= 0")
                     }
-                    Type::String => format!("{this} ~= \"\""),
+                    Type::String => format!("{this} and {this} ~= \"\""),
                     Type::Bool => this,
-                    Type::Bytes => format!("buffer.len({this}) > 0"),
+                    Type::Bytes => format!("{this} and buffer.len({this}) > 0"),
                     Type::Enum => format!(
-                        "{this} ~= 0 or {this} ~= {}.fromNumber(0)",
+                        "{this} ~= nil and ({this} ~= 0 or {this} ~= {}.fromNumber(0))",
                         type_definition_of_field_descriptor(field, self.export_map, self.base_file)
                     ),
                     Type::Message => unreachable!("Message has presence"),
